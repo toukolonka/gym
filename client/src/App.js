1a3d04d31ac1ae084caaac2aacf75e245f62137f
Your Name <you@example.com>
2021-09-17 12:31:33 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [notification, setNotification] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/api/hello')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotification(response.data);
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      {notification}
+    </div>
+  );
+}
+
+export default App;
