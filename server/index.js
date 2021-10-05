059d31cb1b252fa87678487a53445ced8f76e72d
Your Name <you@example.com>
2021-10-05 19:53:02 +0300

    Configured ESLint (BE)

--- server/index.js
+++ server/index.js
@@ -1,34 +1,34 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
-const Exercise = require('./models/exercise')
+require('dotenv').config();
+const Exercise = require('./models/exercise');
 
 const app = express();
-app.use(express.static('build'))
-app.use(cors())
-app.use(express.json())
+app.use(express.static('build'));
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
 
-app.use(requestLogger)
+app.use(requestLogger);
 
-app.get("/api/hello", (_, response) => {
-  response.send("Hello from server!" );
+app.get('/api/hello', (_, response) => {
+  response.send('Hello from server!');
 });
 
 app.get('/api/exercises', (_, response) => {
-  Exercise.find({}).then(exercises => {
-    response.json(exercises)
-  })
-})
+  Exercise.find({}).then((exercises) => {
+    response.json(exercises);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
--- server/models/exercise.js
+++ server/models/exercise.js
@@ -1,26 +1,29 @@
-const mongoose = require('mongoose')
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+
+const mongoose = require('mongoose');
 
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url)
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    console.log('error connecting to MongoDB:', error.message);
   });
 
 const exerciseSchema = new mongoose.Schema({
   name: String,
   description: String,
-})
+});
 
 exerciseSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Exercise', exerciseSchema)
+module.exports = mongoose.model('Exercise', exerciseSchema);
