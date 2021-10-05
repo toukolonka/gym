1fb75b9b92a9a1f42bba98e0b66293e35a64bf66
Your Name <you@example.com>
2021-10-05 19:31:43 +0300

    Connected to MongoDb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const cors = require('cors');
+require('dotenv').config()
+const Exercise = require('./models/exercise')
+
+const app = express();
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-app.use(cors());
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-app.get("/api/hello", (req, res) => {
-    res.send("Hello from server!" );
-  });
+app.use(requestLogger)
+
+app.get("/api/hello", (_, response) => {
+  response.send("Hello from server!" );
+});
+
+app.get('/api/exercises', (_, response) => {
+  Exercise.find({}).then(exercises => {
+    response.json(exercises)
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
---
+++ server/models/exercise.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  });
+
+const exerciseSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+})
+
+exerciseSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Exercise', exerciseSchema)
