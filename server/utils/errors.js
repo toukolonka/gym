0b16e0fb1ead18bdab508a49df45cb04039666e9
Boris Hyttinen <borishyttinen@Boriss-MacBook-Air.local>
2021-10-26 17:09:11 +0300

    First custom errors specified

---
+++ server/utils/errors.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-classes-per-file */
+
+class CustomError extends Error {
+  constructor(message) {
+    super(message);
+    // Ensure the name of this error is the same as the class name
+    this.name = this.constructor.name;
+    // This clips the constructor invocation from the stack trace.
+    // It's not absolutely essential, but it does make the stack trace a little nicer.
+    //  @see Node.js reference (bottom)
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class AuthorizationHeaderError extends CustomError {
+  constructor(message, header) {
+    super(message);
+    this.header = header;
+  }
+}
+
+class InvalidTokenError extends CustomError {
+  constructor(message, header) {
+    super(message);
+    this.header = header;
+  }
+}
+
+module.exports = {
+  AuthorizationHeaderError,
+  InvalidTokenError,
+};
