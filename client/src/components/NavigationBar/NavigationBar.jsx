f324cf5bfdb44d72bc8daa0121ba113a69148a19
Touko Lonka <touko.lonka@gmail.com>
2021-10-28 11:45:37 +0300

    Modified navbar

--- client/src/components/NavigationBar/NavigationBar.jsx
+++ client/src/components/NavigationBar/NavigationBar.jsx
@@ -10,13 +10,13 @@ const NavigationBar = () => (
   <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
     <AppBar position="static" enableColorOnDark color="primary">
       <Toolbar>
-        <Typography variant="h5" component="h5" sx={{ flexGrow: 1 }} color="inherit">
+        <Typography variant="h5" sx={{ flexGrow: 1 }} color="inherit">
           Gym App
         </Typography>
-        <Button color="inherit" component={Link} to="/">Home</Button>
-        <Button color="inherit" component={Link} to="/workouts">Workouts</Button>
-        <Button color="inherit" component={Link} to="/exercises">Exercises</Button>
         <Button color="inherit" component={Link} to="/templates">Templates</Button>
+        <Button color="inherit" component={Link} to="/workouts">Workouts</Button>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/exercises">Exercises</Button>
         <Button color="inherit" component={Link} to="/profile">Profile</Button>
       </Toolbar>
     </AppBar>
