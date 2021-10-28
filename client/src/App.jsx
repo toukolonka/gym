c75b5f0acecba1bde958af7c4a7fa2350c4d18ac
Touko Lonka <touko.lonka@gmail.com>
2021-10-28 14:10:54 +0300

    Heroku

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 
 import SignIn from './pages/SignIn/SignIn';
@@ -11,23 +13,27 @@ import Templates from './pages/Templates/Templates';
 import Profile from './pages/Profile/Profile';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import NavigationBarMobile from './components/NavigationBar/NavigationBarMobile';
+import theme from './theme';
 
 const App = () => (
-  <div className="App">
-    <NavigationBar />
-    <NavigationBarMobile />
-    <Container maxWidth="lg">
-      <Switch>
-        <Route exact path="/sign-in" component={SignIn} />
-        <Route exact path="/sign-up" component={SignUp} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/workouts" component={Workouts} />
-        <Route exact path="/exercises" component={Exercises} />
-        <Route exact path="/templates" component={Templates} />
-        <Route exact path="/profile" component={Profile} />
-      </Switch>
-    </Container>
-  </div>
+  <Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <NavigationBar />
+        <NavigationBarMobile />
+        <Container maxWidth="lg">
+          <Route exact path="/sign-in" component={SignIn} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/workouts" component={Workouts} />
+          <Route exact path="/exercises" component={Exercises} />
+          <Route exact path="/templates" component={Templates} />
+          <Route exact path="/profile" component={Profile} />
+        </Container>
+      </div>
+    </ThemeProvider>
+  </Router>
 );
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './App';
-import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
