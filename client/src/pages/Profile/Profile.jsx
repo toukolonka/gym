7514942ac3c02cd208d7caecec469339aa4a1b70
Touko Lonka <touko.lonka@gmail.com>
2021-10-28 11:52:50 +0300

    Modified profile page links

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const Profile = () => (
   <>
     <Typography variant="h3" component="h3">
       Profile Page
     </Typography>
-    <Grid container>
-      <Grid item>
-        <Link href="/sign-in" variant="body2">
-          Sign In
-        </Link>
-      </Grid>
-      <Grid item>
-        <Link href="/sign-up" variant="body2" color="secondary">
-          Sign Up
-        </Link>
-      </Grid>
-    </Grid>
+    <Button color="primary" component={Link} to="/sign-in">Sign In</Button>
+    <Button color="secondary" component={Link} to="/sign-up">Sign Up</Button>
   </>
 );
 
