9fe88098ea2feebcfdb45cbb7bc0961a099bfad8
Boris Hyttinen <boris.hyttinen@gmail.com>
2021-11-18 23:27:08 +0200

    1RM report working

--- client/src/components/Home/ProgressChart.jsx
+++ client/src/components/Home/ProgressChart.jsx
@@ -4,20 +4,63 @@ import Chart from 'react-google-charts';
 
 const ProgressChart = ({
   data,
-}) => (
-  <Chart
-    height={400}
-    chartType="LineChart"
-    loader={<div>Loading Chart</div>}
-    data={data}
-    options={{
-      hAxis: {
-        title: 'Time',
-      },
-    }}
-  />
-);
+}) => {
+  console.log(data);
+  if (data.length < 2) return <p>No data</p>;
+  return (
+    <Chart
+      height={400}
+      chartType="LineChart"
+      loader={<div>Loading Chart</div>}
+      data={data}
+      options={{
+        title: '1RM Progress',
+        colors: ['#00bcd4'],
+        chartArea: { width: '75%', height: '75%' },
+        backgroundColor: '#303030',
+        hAxis: {
+          title: 'Time',
+          format: 'dd/MM/yy',
+          textStyle: {
+            color: '#fff',
+          },
+          titleTextStyle: {
+            color: '#fff',
+          },
+        },
+        titleTextStyle: {
+          color: '#fff',
+        },
+        vAxis: {
+          textStyle: {
+            color: '#fff',
+          },
+          titleTextStyle: {
+            color: '#fff',
+          },
+        },
+        legend: {
+          textStyle: {
+            color: '#fff',
+          },
+          position: 'bottom',
+        },
+      }}
+    />
+  );
+};
 
 ProgressChart.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  data: PropTypes.arrayOf(PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.instanceOf(Date),
+    ]),
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]),
+  )).isRequired,
 };
+
+export default ProgressChart;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  Box, Typography, Button, Container,
+  Box, Typography, Button, Container, Autocomplete, TextField,
 } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 
 import workoutService from '../../services/workoutService';
+import exerciseService from '../../services/exerciseService';
+import ProgressChart from '../../components/Home/ProgressChart';
 
 const Home = () => {
   const history = useHistory();
+  const [exerciseOptions, setExerciseOptions] = useState([]);
+  const [selectedExercise, setSelectedExercise] = useState(null);
+  const [exerciseData, setExerciseData] = useState([]);
+
+  useEffect(async () => {
+    const exercises = await exerciseService.getAll();
+    setExerciseOptions(exercises);
+  }, []);
 
   const handleStartWorkout = (event) => {
     event.preventDefault();
@@ -17,6 +27,38 @@ const Home = () => {
         history.push(`/workouts/${createdWorkout.id}`);
       });
   };
+
+  useEffect(async () => {
+    const workouts = await workoutService.getAll();
+
+    // If workouts list is empty
+    if (!workouts || !selectedExercise) {
+      setExerciseData([]);
+      return;
+    }
+
+    const draftData = [[selectedExercise.name, '1RM']];
+
+    // If there are workouts
+    await Object.values(workouts).forEach((workout) => {
+      const workoutdate = new Date(workout.date);
+
+      // Get the sets for the particular exercise
+      const exerciseSets = Object.values(workout.sets)
+        .filter((set) => set.exercise.id === selectedExercise.id);
+
+      // Extract best rms from sets
+      const rms = exerciseSets
+        .map(
+          (set) => set.weight * (1 + (set.repetitions / 30)),
+        );
+      const maximum = Math.round(Math.max(...rms));
+      console.log(maximum);
+      // Add the maximum and date to draftData
+      draftData.push([workoutdate, maximum]);
+    });
+    setExerciseData(draftData);
+  }, [selectedExercise]);
 
   return (
     <Container maxWidth="xs">
@@ -53,6 +95,22 @@ const Home = () => {
         <Typography component="h3" variant="h3">
           Statistics
         </Typography>
+        <Autocomplete
+          disablePortal
+          fullWidth
+          value={selectedExercise}
+          options={exerciseOptions}
+          getOptionLabel={(option) => option.name}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          renderInput={(params) => <TextField {...params} label="New Exercise" />}
+          sx={{ mt: 2 }}
+          onChange={(_, exercise) => {
+            if (exercise !== null) {
+              setSelectedExercise(exercise);
+            }
+          }}
+        />
+        <ProgressChart data={exerciseData} />
       </Box>
 
     </Container>
